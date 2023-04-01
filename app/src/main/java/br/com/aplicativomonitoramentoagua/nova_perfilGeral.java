f1ffb1c914ecd27196f9dafa126e73f51bd0bfc8
Maicon Dequicy <cmaicon543@gmail.com>
2023-03-31 20:26:57 -0400

    Implementação de Gráficos

--- app/src/main/java/br/com/aplicativomonitoramentoagua/nova_perfilGeral.java
+++ app/src/main/java/br/com/aplicativomonitoramentoagua/nova_perfilGeral.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.location.GnssAntennaInfo;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,6 +17,15 @@ import androidx.appcompat.widget.Toolbar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
@@ -27,7 +37,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import org.eazegraph.lib.charts.ValueLineChart;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class nova_perfilGeral extends AppCompatActivity{
 
@@ -45,9 +57,8 @@ public class nova_perfilGeral extends AppCompatActivity{
     private TextView resultadoSensor;
     private DatabaseReference ref2;
     private BottomNavigationView navigationView;
-
-    private ValueLineChart valueLineChart;
-
+    private float val = 1f;
+    private LineChart lineChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -82,7 +93,66 @@ public class nova_perfilGeral extends AppCompatActivity{
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
+        //pega id
+        lineChart = (LineChart) findViewById(R.id.grafTemperatura);
+        /*
+        LineDataSet lineDataSet = new LineDataSet(dataValues(), "Temperatura");
+        ArrayList<ILineDataSet> dataSets= new ArrayList<>();
+        dataSets.add(lineDataSet);
+        LineData data = new LineData(dataSets);
+        lineChart.setData(data);
+        lineChart.invalidate();
+        */
 
+        ArrayList<Entry> yValues = new ArrayList<>();
+        yValues.add(new Entry(0, 30f));
+        yValues.add(new Entry(1, 20f));
+        yValues.add(new Entry(2, 10f));
+        yValues.add(new Entry(3, 15f));
+        yValues.add(new Entry(4, 21f));
+
+        LineDataSet set1 = new LineDataSet(yValues, "Temperatura");
+
+        set1.setFillAlpha(110);
+        set1.setLineWidth(3f);
+        set1.setValueTextSize(1f);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(set1);
+
+        LineData data = new LineData(dataSets);
+        lineChart.setData(data);
+
+        String[] dias = new String[]{"Seg", "Ter", "Qua", "Qui", "Sex"};
+
+        XAxis xAxis = lineChart.getXAxis();
+        xAxis.setValueFormatter(new MyAxisFormatter(dias));
+        xAxis.setGranularity(1f);
+    }
+
+   public class MyAxisFormatter extends ValueFormatter {
+       public String[] mdias;
+
+       public MyAxisFormatter(String[] dias) {
+           this.mdias = dias;
+       }
+
+       @Override
+       public String getFormattedValue(float value) {
+           return mdias[(int) value];
+       }
+   }
+
+    //valores a serem plotados
+    private List<Entry> dataValues()
+    {
+        ArrayList<Entry> dataValue = new ArrayList<>();
+        dataValue.add(new Entry(0, 15f));
+        dataValue.add(new Entry(1, 10f));
+        dataValue.add(new Entry(2, 28f));
+        dataValue.add(new Entry(3, 20f));
+        dataValue.add(new Entry(4, 12f));
+        return dataValue;
     }
 
     //pega ids de botoes, layouts e textviews
@@ -99,7 +169,8 @@ public class nova_perfilGeral extends AppCompatActivity{
 
 
     //metodo para pegar o valor mais recente da temperatura
-    public void atualizar(View view) {
+    public void atualizar(View view)
+    {
 
         ref2.addListenerForSingleValueEvent(
                 new ValueEventListener() {
@@ -116,6 +187,7 @@ public class nova_perfilGeral extends AppCompatActivity{
                 }
         );
     }
+
     //mostra mensagem dizendo que os valores foram atualizados
     public void toastAtualizar(View view)
     {
@@ -146,6 +218,4 @@ public class nova_perfilGeral extends AppCompatActivity{
         Intent intent = new Intent(nova_perfilGeral.this, Adicionar_Sensor.class);
         startActivity(intent);
     }
-
-
 }
